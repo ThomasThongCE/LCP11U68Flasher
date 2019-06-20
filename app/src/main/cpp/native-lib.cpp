6d437237bd9419d0fca066eca842552ab8c49ad8
ThomasThongCE <thomasthongCE@gmail.com>
2019-06-20 15:58:36 +0700

    init Project

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,162 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+//#include <cstdio>
+//#include <cstdlib>
+#include <cerrno>
+
+#define TAG "nfc_programmer_jni"
+#define RESET_DRIVER_PATH "/sys/class/gpio-boot-reset/nfc/"
+#define RESET_DRIVER_FILE_PATH "/sys/class/gpio-boot-reset/nfc/mode"
+#define NFC_DEVICE_PATH "/dev/block/sd"
+#define MAXSIZE 256000
+
+
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,    TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,     TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,     TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,    TAG, __VA_ARGS__)
+
+/**
+ * Finding device is mounted on which path (ex: /dev/block/sda, /dev/block/sdb)
+ * @return last character of the path (ex:a, b, c)
+ */
+char findSdx()
+{
+    char name, path[20];
+    size_t len = strlen(NFC_DEVICE_PATH);
+    for (name = 'a'; name <= 'z'; ++name)
+    {
+        strcpy(path, NFC_DEVICE_PATH);
+        path[len] = name;
+        path[len+1] = '\0';
+        if (access(path, F_OK) == 0)
+            return name;
+    }
+    return '\0';
+}
+
+/**
+ * Main function for flashing into <b>lcp11u68</b> <br>
+ * - calculate firmware file size <br>
+ * - load firmware file size <br>
+ * - write down into mcu
+ */
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_nfc_1programmer_MainActivity_program(JNIEnv *env, jobject instance,
+                                                    jstring path_) {
+    DIR *dir;
+    FILE *srcFile;
+    uint32_t fileSize;
+    int fd;
+    ssize_t retVal;
+    const char *path = env->GetStringUTFChars(path_, 0);
+
+    // Check is gpio-boot-reset driver is loaded
+//    if ((dir = opendir(RESET_DRIVER_PATH)) == NULL)
+//    {
+//        LOGE("driver class not available, may be gpio-boot-reset driver not loaded\n");
+//        closedir(dir);
+//        return -1;
+//    }
+//    closedir(dir);
+
+    // Load new update file
+    srcFile = fopen(path, "r");
+    if (srcFile)
+        LOGI("firmware upgrade file %s loaded\n", path);
+    else
+    {
+        LOGE("Can't load firmware upgrade file, error: %s\n", strerror(errno));
+        return -1;
+    }
+
+    // Calculate file size
+    fseek(srcFile, 0, SEEK_END);
+    fileSize = ftell(srcFile);
+    if (!fileSize)
+    {
+        LOGE("Error when calculate firmware upgrade file size: %s", strerror(errno));
+    }
+    if (fileSize > SIZE_MAX)
+    {
+        LOGE("Size of firmware upgrade is greater than size of disk. Stopped upgrade firmware");
+        return -1;
+    }
+    LOGI("Size of firmware upgrade is %d\n", fileSize);
+    fclose (srcFile);
+
+    // Read data from update firmware file (binary mode)
+    fd = open(path, O_RDONLY);
+    if (fd == -1)
+    {
+        LOGE("Can't load firmware upgrade file in binary, error: %s\n", strerror(errno));
+
+        return -1;
+    }
+    auto *buffer = (uint8_t *) malloc(fileSize);
+    retVal = read(fd, buffer, fileSize);
+    if (retVal != fileSize)
+    {
+        free(buffer);
+        close(fd);
+        LOGE ("Read firmware upgrade file error, error: %s\n", strerror(errno));
+        return -1;
+    }
+    close(fd);
+    LOGI ("Read firmware upgrade file complete \n");
+
+    // find device
+    char name = findSdx(), devPath[20];
+    if ('\0' == name)
+    {
+        LOGE ("Can't find sdx file in /dev/block, did nfc boot into programming mode yet?, error: %s\n", strerror(errno));
+        free(buffer);
+        return -1;
+    }
+
+    // find device file
+    size_t len = strlen(NFC_DEVICE_PATH);
+    strcpy(devPath, NFC_DEVICE_PATH);
+    devPath[len] = name;
+    devPath[len+1] = '\0';
+    LOGI ("Device found at %s \n", devPath);
+
+    // open device file
+    fd = open(devPath, O_RDWR);
+    if (fd == -1)
+    {
+        LOGE ("Can't open nfc file %s, error: %s\n", devPath ,strerror(errno));
+        free(buffer);
+        return -1;
+    }
+
+    // Flashing device
+    lseek(fd, 0x800, SEEK_SET);
+    LOGI("Writing firmware data to disk\n");
+    retVal = write(fd, buffer, fileSize);
+    if (retVal != fileSize)
+        LOGE ("file Write only %d, error: %s \n",retVal ,strerror(errno));
+    fsync(fd);
+    close(fd);
+    LOGI("Finished!!!\n");
+
+    //reset_device();
+
+    free (buffer);
+    env->ReleaseStringUTFChars(path_, path);
+    return true;
+}
+
+/**
+ * unmount partition using system call
+ */
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_nfc_1programmer_MainActivity_unmount(JNIEnv *env, jobject instance) {
+    system("umount /mnt/media_rw/0000-0000");
+}
---
+++ app/src/main/java/com/example/nfc_programmer/ResetUtil.java
@@ -0,0 +1,151 @@
+package com.example.nfc_programmer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * ResetUtil class that using <b>gpio-boot-reset</b> to drive <b>lcp11u68</b> into boot mode or normal mode <br>
+ * Verify mcu mode using a usb attach dettach broadcast receiver with <i>wait</i> <i>notify</i> mechanic <br>
+ * normal mode: mcu in running mode <br>
+ * boot mode: mcu in state that can be program <br>
+ */
+public class ResetUtil {
+    private static final String TAG = "ResetUtil: ";
+    private static final String ResetFilePath = "/sys/class/gpio-boot-reset/nfc/mode";
+
+    private static File file;
+    private final Object bootToken;
+    private final Object resetToken;
+    private boolean resetFlag = false, bootFlag = false;
+
+    /**
+     * Init driver file path and token object for verify mcu mode
+     */
+    ResetUtil(){
+        file = new File(ResetFilePath);
+        bootToken = new Object();
+        resetToken = new Object();
+    }
+
+    /**
+     * Drive mcu into boot mode without verify (<i>wait</i> mechanic)
+     * @throws IOException
+     */
+    public void enterProgModeNonBlock() throws IOException {
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.write("prog".getBytes());
+        stream.close();
+    }
+
+    /**
+     * Drive mcu into normal mode without verify (<i>wait</i> mechanic)
+     * @throws IOException
+     */
+    public void enterNormalModeNonBlock() throws IOException {
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.write("normal".getBytes());
+        stream.close();
+    }
+
+    /**
+     * Drive mcu into boot mode with verify (<i>wait</i> mechanic) from broadcast receiver
+     * @return <b>true</b> if reset into bootmode success, <b>false</b> if <i>wait()</i> timeout.
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public boolean enterProgMode() throws IOException, InterruptedException {
+        enterProgModeNonBlock();
+        synchronized (bootToken){
+            bootToken.wait(1000);
+        }
+        if (bootFlag) {
+            Log.i(TAG, "enter programming mode");
+            bootFlag = false;
+            return true;
+        }else
+            return false;
+    }
+
+    /**
+     * Drive mcu into normal mode with verify (<i>wait</i> mechanic) from broadcast receiver
+     * @return <b>true</b> if reset into bootmode success, <b>false</b> if <i>wait()</i> timeout.
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public boolean enterNormalMode() throws IOException, InterruptedException {
+        enterNormalModeNonBlock();
+        synchronized (resetToken){
+            resetToken.wait(1000);
+        }
+        if (resetFlag) {
+            Log.i (TAG, "enter normal mode");
+            resetFlag = false;
+            return true;
+        }else
+            return false;
+
+    }
+
+    /**
+     * Get broadcast receiver
+     * @return broadcast receiver
+     */
+    public BroadcastReceiver getUsbBroadcastReceiver() {
+        return m_BroadCastReceiver;
+    }
+
+    /**
+     * BroadcastReceiver intend to listen to <i>ACTION_USB_DEVICE_ATTACHED</i> and <i>ACTION_USB_DEVICE_DETACHED</i> event <br>
+     * in order to verify mcu in boot/normal mode or not. <br>
+     * Each mode, lcp11u68 have difference <b>VID</b> and <b>PID</b> which allow to verify mcu state <br>
+     */
+    private final BroadcastReceiver m_BroadCastReceiver = new BroadcastReceiver() {
+        private final static int VID_boot = 0x1fc9;
+        private final static int PID_boot = 0x0017;
+        private final static int VID = 0x23eb;
+        private final static int PID = 0x0004;
+
+        /**
+         * Receive <i>ACTION_USB_DEVICE_ATTACHED</i> and <i>ACTION_USB_DEVICE_DETACHED</i> to <i>notify()</i> token which waiting in
+         * <b>enterProgMode</b> and <b>enterNormalMode</b> function
+         * @param context
+         * @param intent
+         */
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            switch (action) {
+                case UsbManager.ACTION_USB_DEVICE_ATTACHED: {
+                    UsbDevice usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    Log.d(TAG, "usb " + Integer.toHexString(usbDevice.getVendorId()) + ":" + Integer.toHexString(usbDevice.getProductId()) + " connected");
+                    synchronized (bootToken) {
+                        if ((usbDevice.getVendorId() == VID_boot) && (usbDevice.getProductId() == PID_boot)) {
+                            bootFlag = true;
+                            bootToken.notify();
+                        }
+                    }
+                    synchronized (resetToken) {
+                        if ((usbDevice.getVendorId() == VID) && (usbDevice.getProductId() == PID)) {
+                            resetFlag = true;
+                            resetToken.notify();
+                        }
+                    }
+                    break;
+                }
+                case UsbManager.ACTION_USB_DEVICE_DETACHED: {
+                    UsbDevice usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    Log.d(TAG, "usb " + Integer.toHexString(usbDevice.getVendorId()) + ":" + Integer.toHexString(usbDevice.getProductId()) + " disconnected");
+                    break;
+                }
+            }
+        }
+    };
+}
